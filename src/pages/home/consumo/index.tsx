9f977f02cad1f85ee41a0f50f22621b75e9880bf
USERSAD\rbarcosm <rbarcosm@everis.com>
2022-05-23 15:19:08 +0200

    add user agent package

--- src/pages/home/consumo/index.tsx
+++ src/pages/home/consumo/index.tsx
@@ -1,7 +1,19 @@
+import { UserAgent, UserAgentProvider } from '@quentin-sommer/react-useragent';
 import React from 'react';
 
 const Consumo: React.FC = () => {
-    return <div className='consumo-page'>Consumo</div>;
+    return (
+        <div className='consumo-page'>
+            <div>CONSUMO</div>
+            <UserAgentProvider ua={window.navigator.userAgent}>
+                <div>
+                    <UserAgent windows>
+                        <p>es windows</p>
+                    </UserAgent>
+                </div>
+            </UserAgentProvider>
+        </div>
+    );
 };
 
 export default Consumo;
--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+// import MobileDetect from 'mobile-detect';
+import React, { useEffect, useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './index.scss';
 
 const Splash: React.FC = () => {
-    //const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     // const goToHome = useCallback(() => {
     //     navigate('/home');
     // }, []);
 
-    // useEffect(() => {
-    //     // Wait for 3 seconds
-    //     setTimeout(() => {
-    //         goToHome();
-    //     }, 1000000);
-    // }, []);
+    //add OnEnded at <ReactPlayer> </ReactPlayer>'goToHome'
+
+    const [playing, setPlaying] = useState<boolean>(false);
+
+    useEffect(() => {
+        setPlaying(true);
+    }, []);
+
     return (
         <div className='splash'>
             <ReactPlayer
@@ -23,8 +26,8 @@ const Splash: React.FC = () => {
                 url='assets/splash.mp4'
                 width='100%'
                 height='100%'
-                playing={true}
-                controls={false}
+                playing={playing}
+                volume={0}
             />
         </div>
     );
